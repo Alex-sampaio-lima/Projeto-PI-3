016872c9374c9ab0ef899ede1a14ef97ea01c622
Kewry Brito <kewrybrito7@gmail.com>
2025-11-20 23:10:06 -0300

    imagens do cardapio funcionado

--- projeto-pi/src/app/components/home-cardapio/main-cardapio/main-cardapio.component.ts
+++ projeto-pi/src/app/components/home-cardapio/main-cardapio/main-cardapio.component.ts
@@ -12,34 +12,34 @@ export class MainCardapioComponent {
 
   // ======== LISTA DE BOLOS =========
   bolos = [
-    { name: 'Brigadeiro',price: 90,description:'Brigadeiro: Delicioso doce de chocolate com granulado.', isFlipped: false },
-    { name: 'Abacaxi', price: 95,description:'Abacaxi: Sabor tropical com pedaços da fruta e toque especial de creme.', isFlipped: false },
-    { name: 'Prestígio', price: 100,description:'Prestígio: Combinação clássica de chocolate e coco.', isFlipped: false },
-    { name: 'Maracujá', price: 110,description:'Maracujá: Sabor cítrico e refrescante com creme suave.', isFlipped: false }
+    { name: 'Brigadeiro',price: 90,description:'Brigadeiro: Delicioso doce de chocolate com granulado.', image: 'assets/Bolos/bolo-chocolate-com-morango-2.png' ,isFlipped: false },
+    { name: 'Abacaxi', price: 95,description:'Abacaxi: Sabor tropical com pedaços da fruta e toque especial de creme.',image: 'assets/Bolos/bolo-abacaxi.png' , isFlipped: false },
+    { name: 'Prestígio', price: 100,description:'Prestígio: Combinação clássica de chocolate e coco.', image: 'assets/Bolos/bolo-de-coco.png', isFlipped: false },
+    { name: 'Maracujá', price: 110,description:'Maracujá: Sabor cítrico e refrescante com creme suave.', image: 'assets/Bolos/bolo-maracuja.jpg', isFlipped: false }
   ];
 
   // ======== LISTA DE Salgados =========
   Salgados = [
-    { name: 'Mini Salgado', price: 75,description:'Mini Salgado: Variedade de mini salgados deliciosos.', isFlipped: false },
-    { name: 'Coxinha', price: 80,description:'Coxinha: Coxinhas crocantes e recheadas.', isFlipped: false },
-    { name: 'Kibe', price: 80,description:'Kibe: Kibes tradicionais e saborosos.', isFlipped: false },
-    { name: 'Pastel', price: 65,description:'Pastel: Pastéis fresquinhos e recheados.', isFlipped: false }
+    { name: 'Mini Salgado', price: 75,description:'Mini Salgado: Variedade de mini salgados deliciosos.',image: 'assets/Salgados/mini-Salgados.png', isFlipped: false },
+    { name: 'Coxinha', price: 80,description:'Coxinha: Coxinhas crocantes e recheadas.', image: 'assets/Salgados/coxinha-2.jpg', isFlipped: false },
+    { name: 'Kibe', price: 80,description:'Kibe: Kibes tradicionais e saborosos.', image: 'assets/Salgados/Kibe.png', isFlipped: false },
+    { name: 'Pastel', price: 65,description:'Pastel: Pastéis fresquinhos e recheados.', image: 'assets/Salgados/pastel.png', isFlipped: false }
   ];
 
   // ======== LISTA DE Kit Festa=========
   kitFesta= [
-    { name: 'Namorados', price: 42,description:'Namorados: Kit especial para casais, com itens românticos.', isFlipped: false },
-    { name: 'Decoração', price: 30,description:'Decoração: Itens para deixar sua festa ainda mais bonita.', isFlipped: false },
-    { name: 'Aniversario', price: 55,description:'Aniversario: Kit completo para comemorar aniversários.', isFlipped: false },
-    { name: 'Festa na Caixa', price: 82,description:'Festa na Caixa: Tudo que você precisa para uma festa prática e divertida.', isFlipped: false },
+    { name: 'Namorados', price: 42,description:'Namorados: Kit especial para casais, com itens românticos.', image: 'assets/KitFesta/kit-namoro.jpg' ,isFlipped: false },
+    { name: 'Decoração', price: 30,description:'Decoração: Itens para deixar sua festa ainda mais bonita.',image: 'assets/KitFesta/kit-festa-2.png' , isFlipped: false },
+    { name: 'Aniversario', price: 55,description:'Aniversario: Kit completo para comemorar aniversários.',image: 'assets/KitFesta/kit-festa-3.png' , isFlipped: false },
+    { name: 'Festa na Caixa', price: 82,description:'Festa na Caixa: Tudo que você precisa para uma festa prática e divertida.', image: 'assets/KitFesta/kit-festa-4.png' , isFlipped: false },
   ];
 
   // ======== LISTA DE Doces=========
   doces = [
-    { name: 'Finos', price: 12,description: 'Finos:Trufas e bombons sofisticados, com toque gourmet',isFlipped: false },
-    { name: 'Tradicinais', price: 5,description:'Tradicionais: Brigadeiros e beijinhos que nunca saem de moda.' ,isFlipped: false },
-    { name: 'Diferentes', price: 10,description: 'Diferentes:Sabores criativos e combinações exclusivas.',isFlipped: false },
-    { name: 'Padaria', price: 20,description:'Padaria:Doces fresquinhos, como sonhos, carolinas e mini tortas.' ,isFlipped: false }
+    { name: 'Finos', price: 12,description: 'Finos:Trufas e bombons sofisticados, com toque gourmet',image: 'assets/Doces/doces-finos.png',isFlipped: false },
+    { name: 'Tradicinais', price: 5,description:'Tradicionais: Brigadeiros e beijinhos que nunca saem de moda.', image: 'assets/Doces/tradicionais.png'  ,isFlipped: false },
+    { name: 'Diferentes', price: 10,description: 'Diferentes:Sabores criativos e combinações exclusivas.',image: 'assets/Doces/diferentes.png',isFlipped: false },
+    { name: 'Padaria', price: 20,description:'Padaria:Doces fresquinhos, como sonhos, carolinas e mini tortas.' ,image: 'assets/Doces/sonho.jpg',isFlipped: false }
   ];
 
   // ======== MÉTODO PARA FLIPAR APENAS O CARD CORRETO ========
