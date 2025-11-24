d35e31e08de46d0197974eeceb7b593f26dc0e22
Sthephany Viana da Silva <sthephanyv021@gmail.com>
2025-11-23 21:45:37 -0300

    feat: implement ClientePedidos component with navigation and form integration

--- projeto-pi/src/app/components/cliente-pedidos/cliente-pedidos.component.ts
+++ projeto-pi/src/app/components/cliente-pedidos/cliente-pedidos.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavBarClientePedidosComponent } from "./nav-bar-cliente-pedidos/nav-bar-cliente-pedidos.component";
 import { NavBarAdminPedidosComponent } from "../admin-pedidos/nav-bar-admin-pedidos/nav-bar-admin-pedidos.component";
 import { ContainerFormAdminPedidosComponent } from "../admin-pedidos/container-form-admin-pedidos/container-form-admin-pedidos.component";
+import { ContainerFormClientePedidosComponent } from "./container-form-cliente-pedidos/container-form-cliente-pedidos.component";
 
 @Component({
   selector: 'app-cliente-pedidos',
-  imports: [NavBarAdminPedidosComponent, ContainerFormAdminPedidosComponent, NavBarClientePedidosComponent],
+  imports: [NavBarAdminPedidosComponent, ContainerFormAdminPedidosComponent, NavBarClientePedidosComponent, ContainerFormClientePedidosComponent],
   templateUrl: './cliente-pedidos.component.html',
   styleUrl: './cliente-pedidos.component.css'
 })
